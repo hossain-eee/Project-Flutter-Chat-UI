3a277c4d6f2fc2ce9795b6597f008a375bbe902e
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-11-14 12:25:12 +0600

    Update favorite_contacts.dart
    favorite contacts name and image has been added to home screen

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -1,37 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/model/message_model.dart';
 
 class FavoriteCotacts extends StatelessWidget {
   const FavoriteCotacts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'favorite Contacts',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'favorite Contacts',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.more_horiz,
-                  size: 30,
-                  color: Colors.blueGrey,
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_horiz,
+                    size: 30,
+                    color: Colors.blueGrey,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        )
-      ],
+          //favorite conatact user image and name
+          Container(
+            height: 120,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 10),
+              scrollDirection: Axis.horizontal,
+              itemCount: favorites.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        favorites[index].name,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
