331f664d171e2b79f57f123e1618538b247e8699
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-11-14 09:50:38 +0600

    colors change

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_chat_ui/screen/home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,8 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primaryColor: Colors.red,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red, // Set your primary color here
+        ).copyWith(
+          secondary: const Color(0XFFFEF9EB),
+        ),
       ),
       home: HomeScreen(),
     );
