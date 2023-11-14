ee4883fa442861b1d04a0b5a125fdcb26b885433
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-11-14 16:40:31 +0600

    recent chat user image and text

--- lib/widgets/recent_chat.dart
+++ lib/widgets/recent_chat.dart
@@ -1,25 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/model/message_model.dart';
+
 class RecentChat extends StatelessWidget {
   const RecentChat({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
-              child: ListView.builder(
-                  itemCount: chats.length,
-                  itemBuilder: (context, index) {
-                    return Text(chats[index].sender.name);
-                  }),
-            ),
-          );
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (context, index) {
+              final Message chat = chats[index];
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage(chat.sender.imageUrl),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            chat.sender.name,
+                            style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.45,
+                            child: Text(
+                              chat.text,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.w600,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(chat.time),
+                      const Text('New'),
+                    ],
+                  )
+                ],
+              );
+            }),
+      ),
+    );
   }
 }
