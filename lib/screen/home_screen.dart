98462c1fcab996258fd3ef523877936d8b3bcba3
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-11-14 10:44:55 +0600

    implemented category selector
    below the appbar, Message, Online, Group

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -36,7 +37,11 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+        ],
+      ),
     );
   }
 }
